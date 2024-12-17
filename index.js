094836f2fec5de7c5167ecae89546f2a89129775
chr-lucas <christopherlucas@mail.weber.edu>
2024-12-17 11:26:55 -0700

    implement ping slash command

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-const {Client} = require('discord.js');
-const {token} = require('./config.json');
+const { Client,Events,GatewayIntentBits,SlashCommandBuilder } = require('discord.js');
+const { token } = require('./config.json');
 
-const client = new Client({intents: []});
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.once(Events.ClientReady, c => {
+  console.log(`Logged in as ${c.user.tag}`);
+
+  const scPing = new SlashCommandBuilder()
+      .setName('ping')
+      .setDescription('Replies with "Pong!"');
+
+      const ping = scPing.toJSON();
+      client.application.commands.create(ping, "126136429783941120");
+});
+
+client.on(Events.InteractionCreate, interaction =>
+  {
+    if(!interaction.isChatInputCommand()) return;
+    if(interaction.commandName === "ping")
+    {
+      const reply = `I'm here! Jeez, stop pinging me, ${interaction.user.username}!`
+      interaction.reply(reply);
+    }
+  }
+)
 
 client.login(token);
