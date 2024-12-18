c330d43f15ba52272aa18993b57bceda4ca44264
chr-lucas <christopherlucas@mail.weber.edu>
2024-12-17 20:27:52 -0700

    Update structure for module based bot functionality

--- index.js
+++ index.js
@@ -1,28 +1,58 @@
-const { Client,Events,GatewayIntentBits,SlashCommandBuilder } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client,Collection,Events,GatewayIntentBits,SlashCommandBuilder } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({intents: [GatewayIntentBits.Guilds]});
 
+client.commands = new Collection();
+
+const foldersPath = path.join(__dirname, 'commands');
+const commandFolders = fs.readdirSync(foldersPath);
+
+for (const folder of commandFolders) {
+	const commandsPath = path.join(foldersPath, folder);
+	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+	for (const file of commandFiles) {
+		const filePath = path.join(commandsPath, file);
+		const command = require(filePath);
+		// command dict add K:V(command name, module)
+		if ('data' in command && 'execute' in command) {
+			client.commands.set(command.data.name, command);
+		} else {
+			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+		}
+	}
+}
+
+// Run once on condition 'client is ready'
 client.once(Events.ClientReady, c => {
   console.log(`Logged in as ${c.user.tag}`);
-
-  const scPing = new SlashCommandBuilder()
-      .setName('ping')
-      .setDescription('Replies with "Pong!"');
-
-      const ping = scPing.toJSON();
-      client.application.commands.create(ping, "126136429783941120");
 });
 
-client.on(Events.InteractionCreate, interaction =>
-  {
-    if(!interaction.isChatInputCommand()) return;
-    if(interaction.commandName === "ping")
-    {
-      const reply = `I'm here! Jeez, stop pinging me, ${interaction.user.username}!`
-      interaction.reply(reply);
-    }
-  }
-)
+// Interaction handling for slash commands
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return; // return non-chat driven cmds
 
+	const command = interaction.client.commands.get(interaction.commandName);
+
+	if (!command) { // simple command does not exist catch
+		console.error(`No command matching ${interaction.commandName} was found.`);
+		return;
+	}
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		if (interaction.replied || interaction.deferred) {
+			await interaction.followUp({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		} else {
+			await interaction.reply({ content: 'There was an error while executing this command!', flags: MessageFlags.Ephemeral });
+		}
+	}
+});
+
+
+// Login
 client.login(token);
