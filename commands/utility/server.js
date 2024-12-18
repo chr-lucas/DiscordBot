4da7dc6e92acd22743e70cda52574abf3d6c17c0
chr-lucas <christopherlucas@mail.weber.edu>
2024-12-17 20:32:51 -0700

    Add basic server info slash command "server"

---
+++ commands/utility/server.js
@@ -0,0 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('server')
+		.setDescription('Provides information about the server.'),
+	async execute(interaction) {
+		// .guild is the object representing the server hosting this instance of the bot
+		await interaction.reply(`This server is ${interaction.guild.name} and has ${interaction.guild.memberCount} members.`);
+	},
+};
